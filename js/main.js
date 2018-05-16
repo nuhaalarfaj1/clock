29c567536b6ace703ee4cb85c4a39bd63fcd7be5
NUHA ALARFAJ <nuhaalarfaj@NUHAs-MacBook-Pro.local>
2018-05-15 19:11:50 -0700

    first

--- js/main.js
+++ js/main.js
@@ -1,45 +1,85 @@
-function clock(){
+var secondsRemaining;
+var intervalHandle;
 
- var today = new Date();
-  
-  var hours = today.getHours();
-  var minutes = today.getMinutes();
-  var seconds = today.getSeconds();
-  
-  
-  if (hours >= 12){
-    meridiem = " PM";
-  }
-  else {
-    meridiem = " AM";
-  }
- 
-  if (hours>12){
-    hours = hours - 12;
-  }
-  else if (hours===0){
-    hours = 12;	
-  }
-  
-  if (minutes<10){
-    minutes = "0" + minutes;
-  }
-  else {
-    minutes = minutes;
-  }
-  
-  if (seconds<10){
-    seconds = "0" + seconds;
-  }
-  else {
-    seconds = seconds;
-  }
-  
-  
-  document.getElementById("clock").innerHTML = (hours + ":" + minutes + ":" + seconds + meridiem);
-  
-  }
-  
-  
-  setInterval('clock()', 1000);
-  
+function resetPage(){
+
+	document.getElementById("inputArea").style.display = "block";
+
+}
+
+function tick(){
+
+	var timeDisplay = document.getElementById("time");
+
+
+	var min = Math.floor(secondsRemaining / 60);
+	var sec = secondsRemaining - (min * 60);
+
+
+	if (sec < 10) {
+		sec = "0" + sec;
+	}
+
+
+	var message = min.toString() + ":" + sec;
+
+
+	timeDisplay.innerHTML = message;
+
+
+	if (secondsRemaining === 0){
+		alert("Done!");
+		clearInterval(intervalHandle);
+		resetPage();
+	}
+
+
+	secondsRemaining--;
+
+}
+
+function startCountdown(){
+
+	function resetPage(){
+		document.getElementById("inputArea").style.display = "block";
+	}
+
+	var minutes = document.getElementById("minutes").value;
+	
+
+	if (isNaN(minutes)){
+		alert("Please enter a number");
+		return; 
+	}
+
+
+	secondsRemaining = minutes * 60;
+	
+	intervalHandle = setInterval(tick, 1000);
+
+
+	document.getElementById("inputArea").style.display = "none";
+
+
+}
+
+window.onload = function(){
+
+
+	var inputMinutes = document.createElement("input");
+	inputMinutes.setAttribute("id", "minutes");
+	inputMinutes.setAttribute("type", "text");
+	
+
+	var startButton = document.createElement("input");
+	startButton.setAttribute("type","button");
+	startButton.setAttribute("value","Start Countdown");
+	startButton.onclick = function(){
+		startCountdown();
+	};
+
+	document.getElementById("inputArea").appendChild(inputMinutes);
+	document.getElementById("inputArea").appendChild(startButton)		
+
+}
+
